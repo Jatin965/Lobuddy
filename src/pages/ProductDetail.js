663ceac8641aec3111b26adfd56dcbbbd3ff0859
Jatin Saini <jatin4762@gmail.com>
2021-09-05 22:55:01 +0530

    Edit Product page

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -8,6 +8,8 @@ import { detailProduct, listProducts } from "../redux/actions/productActions";
 import Faq from "../components/UI/Faq";
 import Product from "../components/Cards/Product";
 
+import Customer from "../assets/images/home/cus.png";
+
 const ProductDetail = () => {
   const dispatch = useDispatch();
   const match = useRouteMatch();
@@ -30,9 +32,9 @@ const ProductDetail = () => {
     dispatch(listProducts());
   }, [dispatch]);
 
-  // if (loading || loadingDetail) {
-  //   return <h1> Loading ...</h1>;
-  // }
+  if (loading || loadingDetail) {
+    return <h1> Loading ...</h1>;
+  }
 
   if (error || errorDetail) {
     return <h1>Error</h1>;
@@ -40,12 +42,37 @@ const ProductDetail = () => {
 
   return (
     <div className="detail">
-      <Faq />
+      <div className="detail-product">
+        <h2>Product Details</h2>
+        {product.description}
+        <br />
+        {product.details}
+      </div>
 
-      <div className="most-products">
-        {products.slice(10, 14).map((product) => (
-          <Product product={product} />
-        ))}
+      <div className="sec3">
+        <div className="what">
+          <h2>What you'll get in the box</h2>
+          <div className="what-content"></div>
+        </div>
+        <Faq />
+      </div>
+
+      <div className="testimonials">
+        <div className="content">
+          <h1>Our happy customers</h1>
+          <p>Check out what our customer saying about us</p>
+        </div>
+        <img src={Customer} alt="Customer" />
+      </div>
+
+      <div className="detail-most">
+        <h1>Our best deals</h1>
+
+        <div className="most-products">
+          {products.slice(9, 13).map((product) => (
+            <Product product={product} />
+          ))}
+        </div>
       </div>
     </div>
   );
