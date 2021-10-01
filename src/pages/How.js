0d3e21a4535d70b42d48f716dce9d826012c039d
jatin <jatin4762@gmail.com>
2021-10-01 10:55:22 +0530

    How complete

--- src/pages/How.js
+++ src/pages/How.js
@@ -16,13 +16,19 @@ import im1 from "../assets/images/how/1.png";
 import im2 from "../assets/images/how/2.png";
 import im3 from "../assets/images/how/3.png";
 
+import Up from "../assets/images/how/up.png";
+import Buddy from "../assets/images/how/buddy.png";
+import Eco from "../assets/images/how/eco.png";
+import Mini from "../assets/images/how/mini.png";
+// import Up from '../assets/images/how/up.png'
+
 const How = () => {
   return (
     <div className="how">
       <div className="how-header">
         <CircleBag child={<QuestionOutlined className="how-icon" />} />
 
-        <h1>How it works!</h1>
+        <h1 style={{ fontFamily: "Arial Rounded MT" }}>How it works!</h1>
       </div>
       <div className="how-1">
         <img src={im1} alt="display-1" />
@@ -49,52 +55,31 @@ const How = () => {
       <div className="how-header how-benefits">
         <CircleBag child={<StarOutlined className="how-icon" />} />
 
-        <h1>Your Benefits</h1>
+        <h1 style={{ fontFamily: "Arial Rounded MT" }}>Your Benefits</h1>
       </div>
       <div className="grid-container">
         <div className="benefit ben-1">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: ani1,
-            }}
-            style={{ margin: 0 }}
-            width={400}
-            height={350}
-          ></Lottie>
+          <div className="img-container" style={{ padding: "2rem 5rem" }}>
+            <img src={Up} alt="Upgrade" />
+          </div>
           <div className="ben-content">
             <h1>Easy upgrade</h1>
             <p>Trendy technology accessibility in very minimal price</p>
           </div>
         </div>
         <div className="benefit ben-2">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: ani2,
-            }}
-            style={{ margin: 0 }}
-            width={400}
-            height={350}
-          ></Lottie>
+          <div className="img-container">
+            <img src={Eco} alt="Economic" />
+          </div>
           <div className="ben-content">
             <h1>Pocket friendly</h1>
             <p>No deposit, only rental amount</p>
           </div>
         </div>
         <div className="benefit ben-3">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: ani3,
-            }}
-            style={{ margin: 0 }}
-            width={400}
-            height={350}
-          ></Lottie>
+          <div className="img-container" style={{ padding: "2rem 5rem" }}>
+            <img src={Up} alt="Upgrade" />
+          </div>
           <div className="ben-content">
             <h1>Stay flexible</h1>
             <p>
@@ -104,32 +89,18 @@ const How = () => {
           </div>
         </div>
         <div className="benefit ben-4">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: ani4,
-            }}
-            style={{ margin: 0 }}
-            width={400}
-            height={350}
-          ></Lottie>
+          <div className="img-container">
+            <img src={Buddy} alt="Buddy" />
+          </div>
           <div className="ben-content">
             <h1>Rent from buddy</h1>
             <p>Commitment and hassle free renting</p>
           </div>
         </div>
         <div className="benefit ben-5">
-          <Lottie
-            options={{
-              loop: true,
-              autoplay: true,
-              animationData: ani5,
-            }}
-            style={{ margin: 0 }}
-            width={400}
-            height={350}
-          ></Lottie>
+          <div className="img-container">
+            <img src={Mini} alt="Mini rental" />
+          </div>
           <div className="ben-content">
             <h1>Mini renting</h1>
             <p>Rental periods starts from one month</p>
